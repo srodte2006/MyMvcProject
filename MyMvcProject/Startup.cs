ea49eee7bed8a53c5a5d09a56e8836cd4af5c40a
Abay, Tedros A <taabay@student.rtc.edu>
2019-05-22 22:03:53 -0700

    Add project files.

---
+++ MyMvcProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MyMvcProject.Startup))]
+namespace MyMvcProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
